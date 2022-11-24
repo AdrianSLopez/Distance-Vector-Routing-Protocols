82197f2e82f9926a7e9f780a3f92895275332199
Nep2un <jupi2erinspace@gmail.com>
2022-11-23 16:23:50 -0800

    Create foundation of program by handling userInput.

--- Server.java
+++ Server.java
@@ -7,12 +7,13 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Server extends Thread{ 
-    private static int port = 8080;
+    private static int port = 2000;
     private static ServerSocket serverSocket;
     private static Socket clientSocket;
     private static BufferedReader in;
     private static PrintWriter out;
     private static Scanner s = new Scanner(System.in);
+    private static boolean startupCmdEntered = false;
 
     public Server(int port) {
         Server.port = port;
@@ -22,27 +23,27 @@ public class Server extends Thread{
         try {
             serverSocket = new ServerSocket(port);
 
-            System.out.println("SERVER RUNNING....");
+            System.out.println("SERVER STARTED");
 
             clientSocket = serverSocket.accept();
 
             out = new PrintWriter(clientSocket.getOutputStream());
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            Thread sender = new Thread(new Runnable() {
-                String message;
+            // To do steps
+                // Send following to clients:
+                    // Intro message along with startup command info
+                        // be able to display command info
+                        // handle user input in receive thread
+                    // Create nodes from topology.txt
+                    // Aswell with other Proj 2 requirements. Example: Update based on user input
+                    // Display and listen for user avaailable commands
+                    // Execute based on user command input
+            
+            out.println(introMsg());
+            out.flush();
 
-                @Override
-                public void run() {
-                    while(true) {
-                        message = s.nextLine();
-                        out.println(message);
-                        out.flush();
-                    }
-                }
-            });
-            sender.start();
-
+            // Server receives from the client from the following
             Thread receive = new Thread(new Runnable() {
                 String msg ;
                 @Override
@@ -51,7 +52,9 @@ public class Server extends Thread{
                         msg = in.readLine();
 
                         while(msg!=null){
-                            System.out.println("Client : " + msg);
+                            System.out.println(msg);
+                            out.println(executeCommand(msg.substring(8)));
+                            out.flush();
                             msg = in.readLine();
                         }
 
@@ -74,8 +77,64 @@ public class Server extends Thread{
         }
     }
 
-    public int getPort() {
-        return Server.port;
+    private static String executeCommand(String userInput) {
+        switch(userInput) {
+            case "help": 
+                return (!startupCmdEntered)? help1(): help2();
+            case "server":
+                //READ TOPOLOGY FILE AND TIME INTERVAL
+                    // Create node class
+                    // for every server inputted in .txt file create node object
+                    // connect to each server?? I think
+                return "";
+            
+            default:
+                return "\u001B[31m" + "Invalid input" + "\u001B[0m";
+        }
     }
 
+    private static String help1() {
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_BLUE = "\u001B[34m";
+        String ANSI_GREEN = "\u001B[32m";
+        String param1 = ANSI_GREEN + " <topology file>" + ANSI_RESET;
+        String param2 = ANSI_BLUE + " <routing-update-interval> " + ANSI_RESET;
+
+        return """
+             server -t """ + param1 + " " + """
+                     -i """ + param2 + "\n\n         " + param1 + """
+                                             The topology file contains the initial topology configuration for the server, e.g., timberlake_init.txt. 
+                             """ + "         " + param2 + """
+                                          It specifies the time interval between routing updates in seconds.
+                                     """;
+    }
+
+    private static String help2() {
+        return """
+            update    .....
+            step      .....
+            packets   .....
+            display   .....
+            crash     .....
+                                     """;
+    }
+
+    private static String introMsg() {
+        return 
+        """
+                             WELCOME TO
+        ===========================================================
+        |                                                         |  
+        |    SSSSSS  EEEEEE  RRRRR   V       V EEEEEE  RRRRR      |
+        |    S       E       R    R   V     V  E       R    R     |
+        |    SSSSSS  EEEE    RRRRR     V   V   EEEE    RRRRR      |
+        |         S  E       R    R     V V    E       R    R     |
+        |    SSSSSS  EEEEEE  R     R     V     EEEEEE  R     R    |
+        |                                                         |
+        |                                              Port:""" + 2000 + """
+          |
+        =========================================================== 
+         Use 'help' command to view a list of available command(s).                                                   
+                """;
+    }
 }
