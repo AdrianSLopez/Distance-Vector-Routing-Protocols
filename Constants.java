17c47654feba4c1892f6eb54cfff13d9b0d40c52
Nep2un <jupi2erinspace@gmail.com>
2022-11-26 16:04:13 -0800

    O.o Remove IP!!

--- Constants.java
+++ Constants.java
@@ -3,7 +3,7 @@ public final class Constants {
     }
 
     // SERVER INFO
-    public static final String IP = "192.168.1.189";
+    public static final String IP = ""; //Add your ip
     public static final int PORT = 2000;
 
     // COLOR
